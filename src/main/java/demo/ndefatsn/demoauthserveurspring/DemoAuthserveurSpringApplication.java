be2e998bbe385305811e55f7b0cb5a2170e81edb
moustaphisene <32897441+moustaphisene@users.noreply.github.com>
2025-05-29 11:19:45 +0000

    First commit

---
+++ src/main/java/demo/ndefatsn/demoauthserveurspring/DemoAuthserveurSpringApplication.java
@@ -0,0 +1,13 @@
+package demo.ndefatsn.demoauthserveurspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthserveurSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAuthserveurSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/ndefatsn/demoauthserveurspring/DemoAuthserveurSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.ndefatsn.demoauthserveurspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAuthserveurSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
